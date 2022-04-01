78774d0cecb13057501748c18ed02817944d4d29
Elkhan <955pro@mail.ru>
2022-04-02 01:08:11 +0500

    last realizing edit form and add form

--- script/script.js
+++ script/script.js
@@ -31,19 +31,28 @@ const initialCards = [
 const elementsSection = document.querySelector(".elements");
 const cardTemplate = document.querySelector("#card");
 
-// geting edit-button and getting add-button
 const editBtn = document.querySelector(".profile__edit-button");
 const addCardBtn = document.querySelector(".profile__add-button");
 
-// getting popups
 const editPopup = document.querySelector(".popup_edit");
 const addCardPopup = document.querySelector(".popup_card");
 const imagePopup = document.querySelector(".popup_image");
 
-// getting close buttons
 const editPopupClsBtn = editPopup.querySelector(".popup__close-button");
 const addCardPopupClsBtn = addCardPopup.querySelector(".popup__close-button");
 const imagePopupClsBtn = imagePopup.querySelector(".popup__close-button");
+
+// getting forms
+const addCardForm = addCardPopup.querySelector(".popup__form");
+const editForm = editPopup.querySelector(".popup__form");
+
+// getting edit form fields
+const editFormName = editForm.querySelector(".popup__item-name");
+const editFormDescription = editForm.querySelector(".popup__item-descrption");
+
+// getting profile elements
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
 /**********************************************************
  * MAIN CODE
@@ -58,6 +67,51 @@ for (let i = 0; i < initialCards.length; i = i + 1) {
 }
 
 /**********************************************************
+ * ADDING LISTENERS
+ **********************************************************/
+
+// filling form fields with profile data
+editBtn.addEventListener("click", function () {
+  editFormName.value = profileTitle.textContent;
+  editFormDescription.value = profileSubtitle.textContent;
+  togglePopup(editPopup);
+});
+
+addCardBtn.addEventListener("click", function () {
+  togglePopup(addCardPopup);
+});
+
+editPopupClsBtn.addEventListener("click", function () {
+  togglePopup(editPopup);
+});
+
+addCardPopupClsBtn.addEventListener("click", function () {
+  togglePopup(addCardPopup);
+});
+
+addCardForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = addCardForm.querySelector(".popup__item-name").value;
+  const link = addCardForm.querySelector(".popup__item-descrption").value;
+  const newCard = createCard(name, link);
+
+  elementsSection.prepend(newCard);
+  togglePopup(addCardPopup);
+  addCardForm.reset();
+});
+
+// handle edit form
+editForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  profileTitle.textContent = editFormName.value;
+  profileSubtitle.textContent = editFormDescription.value;
+
+  togglePopup(editPopup);
+});
+
+/**********************************************************
  * FUNCTIONS
  **********************************************************/
 
@@ -65,61 +119,32 @@ function createCard(name, link) {
   const newCard = cardTemplate.content.cloneNode(true);
   const newCardImage = newCard.querySelector(".elements__image");
   const newCardTitle = newCard.querySelector(".elements__title");
-  // getting like button
   const newCardLikeBtn = newCard.querySelector(".elements__like-button");
-  // getting trash button
   const newCardTrashBtn = newCard.querySelector(".elements__trash");
 
   newCardImage.src = link;
   newCardImage.alt = name;
   newCardTitle.textContent = name;
 
-  /**********************************************************
-   * ADDING LISTENERS
-   **********************************************************/
-
-  // adding event listener for like button
   newCardLikeBtn.addEventListener("click", function () {
     toggleLike(newCardLikeBtn);
   });
 
-  //adding event listener for trash button
   newCardTrashBtn.addEventListener("click", function () {
-    DeleteElement(newCardTrashBtn.closest(".elements__item"));
-  });
-
-  //adding event listeners for edit-popup and for add-popup
-  editBtn.addEventListener("click", function () {
-    togglePopup(editPopup);
-  });
-
-  addCardBtn.addEventListener("click", function () {
-    togglePopup(addCardPopup);
-  });
-
-  //adding event listeners for popup-close buttons
-  editPopupClsBtn.addEventListener("click", function () {
-    togglePopup(editPopup);
-  });
-  
-  addCardPopupClsBtn.addEventListener("click", function () {
-    togglePopup(addCardPopup);
+    deleteElement(newCardTrashBtn.closest(".elements__item"));
   });
 
   return newCard;
 }
 
-// function for like buttons
 function toggleLike(button) {
   button.classList.toggle("elements__like-button_active");
 }
 
-//function for delete DOM-elements
-function DeleteElement(elem) {
+function deleteElement(elem) {
   elem.remove();
 }
 
-// function for open/close a popup
 function togglePopup(popup) {
-  popup.toggle.classList("popup__opened");
+  popup.classList.toggle("popup_opened");
 }
