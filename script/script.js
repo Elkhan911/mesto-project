eba9196a391a28bb083afe430dde72ecb1e69b9a
Elkhan <955pro@mail.ru>
2022-04-01 20:34:57 +0500

    adding card from array + html

---
+++ script/script.js
@@ -0,0 +1,60 @@
+/**********************************************************
+ * VARIABLES
+ **********************************************************/
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const elementsSection = document.querySelector(".elements");
+const cardTemplate = document.querySelector("#card");
+
+/**********************************************************
+ * MAIN CODE
+ **********************************************************/
+
+// adding cards from initialCards array:
+for (let i = 0; i < initialCards.length; i = i + 1) {
+  const cardName = initialCards[i].name;
+  const cardLink = initialCards[i].link;
+  const card = createCard(cardName, cardLink);
+  elementsSection.append(card);
+}
+
+/**********************************************************
+ * FUNCTIONS
+ **********************************************************/
+
+function createCard(name, link) {
+  const newCard = cardTemplate.content.cloneNode(true);
+  const newCardImage = newCard.querySelector(".elements__image");
+  const newCardTitle = newCard.querySelector(".elements__title");
+
+  newCardImage.src = link;
+  newCardImage.alt = name;
+  newCardTitle.textContent = name;
+
+  return newCard;
+}
