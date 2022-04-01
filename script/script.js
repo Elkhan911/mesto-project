953b20e1a012440a5adee4f544972766ac744e56
Elkhan <955pro@mail.ru>
2022-04-02 00:04:26 +0500

    realizing popup forms with close buttons

--- script/script.js
+++ script/script.js
@@ -31,6 +31,20 @@ const initialCards = [
 const elementsSection = document.querySelector(".elements");
 const cardTemplate = document.querySelector("#card");
 
+// geting edit-button and getting add-button
+const editBtn = document.querySelector(".profile__edit-button");
+const addCardBtn = document.querySelector(".profile__add-button");
+
+// getting popups
+const editPopup = document.querySelector(".popup_edit");
+const addCardPopup = document.querySelector(".popup_card");
+const imagePopup = document.querySelector(".popup_image");
+
+// getting close buttons
+const editPopupClsBtn = editPopup.querySelector(".popup__close-button");
+const addCardPopupClsBtn = addCardPopup.querySelector(".popup__close-button");
+const imagePopupClsBtn = imagePopup.querySelector(".popup__close-button");
+
 /**********************************************************
  * MAIN CODE
  **********************************************************/
@@ -59,6 +73,11 @@ function createCard(name, link) {
   newCardImage.src = link;
   newCardImage.alt = name;
   newCardTitle.textContent = name;
+
+  /**********************************************************
+   * ADDING LISTENERS
+   **********************************************************/
+
   // adding event listener for like button
   newCardLikeBtn.addEventListener("click", function () {
     toggleLike(newCardLikeBtn);
@@ -67,6 +86,24 @@ function createCard(name, link) {
   //adding event listener for trash button
   newCardTrashBtn.addEventListener("click", function () {
     DeleteElement(newCardTrashBtn.closest(".elements__item"));
+  });
+
+  //adding event listeners for edit-popup and for add-popup
+  editBtn.addEventListener("click", function () {
+    togglePopup(editPopup);
+  });
+
+  addCardBtn.addEventListener("click", function () {
+    togglePopup(addCardPopup);
+  });
+
+  //adding event listeners for popup-close buttons
+  editPopupClsBtn.addEventListener("click", function () {
+    togglePopup(editPopup);
+  });
+  
+  addCardPopupClsBtn.addEventListener("click", function () {
+    togglePopup(addCardPopup);
   });
 
   return newCard;
@@ -80,4 +117,9 @@ function toggleLike(button) {
 //function for delete DOM-elements
 function DeleteElement(elem) {
   elem.remove();
+}
+
+// function for open/close a popup
+function togglePopup(popup) {
+  popup.toggle.classList("popup__opened");
 }
